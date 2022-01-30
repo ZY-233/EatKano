d172db5af301f944b0434f471410a3247751e1ba
ZY-233 <70693350+ZY-233@users.noreply.github.com>
2022-01-30 14:05:33 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -442,9 +442,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
 
         if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 8) return '太强了~';
+        if (cps <= 10)  return '离谱~';
+        if (cps <= 15) return '别卷啦！';
         return '人？';
     }
 
